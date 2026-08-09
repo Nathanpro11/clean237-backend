27f1dc3d50e4c348233c692b8420fc2084f24543
mbainadji <mbainadjinehemi@gmail.com>
2026-08-09 19:15:03 +0100

    feat(utilisateurs): mise a jour du modele permission

--- src/modules/utilisateurs/models/permission.model.ts
+++ src/modules/utilisateurs/models/permission.model.ts
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
 const permissionSchema = new mongoose.Schema({
-    
-})
+  nom: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true 
+});
+
+export default mongoose.model("Permission", permissionSchema);
