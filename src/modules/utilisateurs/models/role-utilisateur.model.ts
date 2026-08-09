bce2588dba64c66b6bd6e605bea21a53f183f372
mbainadji <mbainadjinehemi@gmail.com>
2026-08-09 19:04:35 +0100

    feat(utilisateurs): mise a jour du modele role-utilisateur

--- src/modules/utilisateurs/models/role-utilisateur.model.ts
+++ src/modules/utilisateurs/models/role-utilisateur.model.ts
@@ -1,5 +1,30 @@
 import mongoose from "mongoose";
 
-const  roleUtilisateurSchema = new mongoose.Schema({
-    
-})
+const roleUtilisateurSchema = new mongoose.Schema({
+  
+  roleId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Role",
+    required: true
+  },
+  
+  utilisateurId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Utilisateur",
+    required: true
+  },
+  assignerPar: {
+    type: String,
+    required: true
+  },
+  dateAssigner: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true
+});
+
+roleUtilisateurSchema.index({ roleId: 1, utilisateurId: 1 }, { unique: true });
+
+export default mongoose.model("RoleUtilisateur", roleUtilisateurSchema);
