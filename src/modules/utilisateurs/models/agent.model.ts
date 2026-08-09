4159037800498cbe0b81c0196e7551a9db272709
mbainadji <mbainadjinehemi@gmail.com>
2026-08-09 19:10:51 +0100

    feat(utilisateurs): mise a jour du modele agent

--- src/modules/utilisateurs/models/agent.model.ts
+++ src/modules/utilisateurs/models/agent.model.ts
@@ -1,5 +1,19 @@
 import mongoose from "mongoose";
+import Utilisateur from "./utilisateur.model";
+
 
 const agentSchema = new mongoose.Schema({
-    
-})
+  zoneAffectee: {
+    type: String,
+    required: true
+  },
+  matricule: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
+
+
+const Agent = Utilisateur.discriminator("Agent", agentSchema);
+export default Agent;
