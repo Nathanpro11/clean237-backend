0fac4ee39f213a6859dd7bdbbac0cbfd8f9fa40e
mbainadji <mbainadjinehemi@gmail.com>
2026-08-10 10:21:53 +0100

    feat(utilisateurs): integration de la ligne de route dans le fichier partage app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+// module Gestion Utilisateur
+import userRoutes from './modules/utilisateurs/routes/utilisateurs.routes';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+// module Gestion Utilisateur
+app.use("/api/utilisateurs", userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API Clean237 OK !');
