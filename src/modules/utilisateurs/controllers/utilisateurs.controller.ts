037144c0f1c0b716f9222e1f998aa1155dcecec2
mbainadji <mbainadjinehemi@gmail.com>
2026-08-10 09:03:38 +0100

    feat(utilisateurs): controleur  -  inscription

--- src/modules/utilisateurs/controllers/utilisateurs.controller.ts
+++ src/modules/utilisateurs/controllers/utilisateurs.controller.ts
@@ -1,89 +1,49 @@
-import { Request, Response, NextFunction } from 'express';
-import { UtilisateurService } from '../services/utilisateur.service';
-import { InscriptionAgentSchema, InscriptionUtilisateurSchema } from '../validators/utilisateurs.validator';
+import type { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import Utilisateur from "../models/utilisateur.model";
+import Agent from "../models/agent.model";
+import { InscriptionUtilisateurSchema, InscriptionAgentSchema } from "../validators/utilisateurs.validator";
 
-export class UtilisateursController {
-  private utilisateurService: UtilisateurService;
 
-  constructor() {
-    this.utilisateurService = new UtilisateurService();
+export const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+   
+    const valideUser = await InscriptionUtilisateurSchema.validateAsync(req.body);
+
+    
+    const user = new Utilisateur(valideUser);
+    await user.save();
+
+    return res.status(201).json({ "message": "User created successfully" });
+  } catch (error: any) {
+    if (error.isJoi) {
+      return next({
+        status: 422,
+        message: error.details.map((err: any) => err.message.replace(/"/g, ""))
+      });
+    }
+    next(error);
   }
+};
 
-  registerUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      
-      const { error, value } = InscriptionUtilisateurSchema.validate(req.body, { abortEarly: false });
-      if (error) {
-        res.status(400).json({ 
-          status: 'Erreur de validation',
-          details: error.details.map(err => err.message) 
-        });
-        return;
-      }
 
-     
-      const nouvelUtilisateur = await this.utilisateurService.creerUtilisateur(value);
-      res.status(201).json({
-        message: 'Utilisateur cree avec succes',
-        data: nouvelUtilisateur
+export const createAgent = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    
+    const valideAgent = await InscriptionAgentSchema.validateAsync(req.body);
+
+    
+    const agent = new Agent(valideAgent);
+    await agent.save();
+
+    return res.status(201).json({ "message": "Agent created successfully" });
+  } catch (error: any) {
+    if (error.isJoi) {
+      return next({
+        status: 422,
+        message: error.details.map((err: any) => err.message.replace(/"/g, ""))
       });
-    } catch (err) {
-      next(err); 
     }
-  };
-
-
-  registerAgent = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      
-      const { error, value } = InscriptionAgentSchema.validate(req.body, { abortEarly: false });
-      if (error) {
-        res.status(400).json({ 
-          status: 'Erreur de validation',
-          details: error.details.map(err => err.message) 
-        });
-        return;
-      }
-
-      
-      const nouvelAgent = await this.utilisateurService.creerAgent(value);
-      res.status(201).json({
-        message: 'Agent cree avec succes',
-        data: nouvelAgent
-      });
-    } catch (err) {
-      next(err); 
-    }
-  };
-
-
-   getAllUsers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      
-      const page = parseInt(req.query.page as string) || 1;
-      const limite = parseInt(req.query.limite as string) || 10;
-
-      
-      const resultat = await this.utilisateurService.listerTout(page, limite);
-      res.status(200).json(resultat);
-    } catch (err) {
-      next(err);
-    }
-  };
-
-
-  getUserById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      const { id } = req.params;
-      
-      // Appel du service pour trouver le profil correspondant
-      const utilisateur = await this.utilisateurService.trouverParId(id);
-      
-      res.status(200).json(utilisateur);
-    } catch (err) {
-      next(err);
-    }
-  };
-
-
-}
+    next(error);
+  }
+};
