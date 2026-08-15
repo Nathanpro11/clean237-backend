be98ae5dfa68134d1863d59ec61771a39f977aeb
Emma-667 <emmanuelletemgoua667@gmail.com>
2026-08-15 11:27:50 +0100

    Ajout de schema de validation pour bacAOrdure

---
+++ src/modules/geolocalisation/utils/validationSchema.ts
@@ -0,0 +1,11 @@
+import Joi from "joi";
+
+export const bacValidationSchema = Joi.object({
+    code: Joi.string().required(),
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
+    etat: Joi.string().optional(),
+    contenance: Joi.number().required(),
+    dateDerniereCollecte: Joi.date().optional(),
+    zone: Joi.string().required(),
+});
