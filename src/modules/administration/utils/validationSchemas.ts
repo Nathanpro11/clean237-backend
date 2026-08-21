08834e01b9ac1c8155e7391f5dec025de22b42b7
Nathanpro11 <nathantchouante77@gmail.com>
2026-08-21 20:26:13 +0100

    feat: ajouter les schémas de validation

--- src/modules/administration/utils/validationSchemas.ts
+++ src/modules/administration/utils/validationSchemas.ts
@@ -1,11 +1,5 @@
 import Joi from 'joi';
 
-export const zoneValidation = Joi.object({
-  nom: Joi.string().required(),
-  quartier: Joi.string().required(),
-  etat: Joi.string().valid('actif', 'inactif').default('actif')
-}); 
-
 export const idValidation = Joi.object({
   id: Joi.string().required()
 });
@@ -13,14 +7,33 @@ export const idValidation = Joi.object({
 export const reportValidation = Joi.object({
   titre: Joi.string().required(),
   description: Joi.string().optional(),
-  zoneId: Joi.string().required(),
-  statistiques: Joi.object({
-    totalAlerts: Joi.number().required(),
-    activeAlerts: Joi.number().required(),
-    resolvedAlerts: Joi.number().required(),
-    totalCollections: Joi.number().required(),
-    completedCollections: Joi.number().required(),
-    pendingCollections: Joi.number().required()
-  }).required()
+  analyseId: Joi.string().required(),
+  statistiques: Joi.object().optional()
 });
 
+export const reportUpdateValidation = Joi.object({
+  titre: Joi.string().optional(),
+  description: Joi.string().optional(),
+  analyseId: Joi.string().optional(),
+  statistiques: Joi.object().optional()
+});
+
+export const analyseValidation = Joi.object({
+  type: Joi.string().required(),
+  periode: Joi.object({
+    start: Joi.date().required(),
+    end: Joi.date().required(),
+  }).required(),
+  donneesUtilisees: Joi.array().items(Joi.string()).optional()  ,
+});
+
+export const analyseUpdateValidation = Joi.object({
+  type: Joi.string().optional(),
+  periode: Joi.object({
+    start: Joi.date().optional(),
+    end: Joi.date().optional(),
+  }).optional(),
+  resultat: Joi.string().optional(),
+  indicateurs: Joi.object().optional(),
+  donneeEnvironnementaleId: Joi.string().optional(),
+});
